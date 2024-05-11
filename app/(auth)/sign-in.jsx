da53b8654cf7de801daf1f6d5324be59519d032f
viper23 <htonnycarlos@gmail.com>
2024-05-11 16:18:12 +0200

    structure for screens

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <View>
+      <Text>sign-in</Text>
+    </View>
+  )
+}
+
+export default signIn
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,47 @@
-import { Slot } from 'expo-router'
-import { StyleSheet, View, Text } from 'react-native'
+import { SplashScreen, Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+
+    const [fontsLoaded, error] = useFonts({
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    });
+
+    useEffect(() => {
+        if (error) throw error;
+
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    if (!fontsLoaded && !error) {
+        return null;
+    }
+
+
     return (
-        <Slot/>
+        // where we declare our screens of our app
+        <Stack >
+            <Stack.Screen name='index' optiion={{ headerShown: 'false' }} />
+            <Stack.Screen name='profile' optiion={{ headerShown: 'false' }} />
+        </Stack >
     )
 }
 
